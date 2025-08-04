d6c8885c576a0af170470160275f483a0e9f107e
whattablackhole <rudolf.gritsenyak@gmail.com>
2025-08-04 14:47:56 +0300

    fix(driver_service): correct tonic include

--- driver_service_rs/src/domain/services/mod.rs
+++ driver_service_rs/src/domain/services/mod.rs
@@ -2,5 +2,5 @@ pub mod trips_finder_service;
 
 
 pub mod fun_taxi_messages_proto_trips {
-    tonic::include_proto!("fun_taxi_messages_proto_trips");
+    tonic::include_proto!("fun_taxi_messages_proto.trips.v1");
 }
