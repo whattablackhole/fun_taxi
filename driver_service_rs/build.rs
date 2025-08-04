46fb256fab6224b7d1131d0e80089f2cecbf834f
whattablackhole <rudolf.gritsenyak@gmail.com>
2025-08-04 20:53:11 +0300

    update(driver_service): tonic v13 -> v14

--- driver_service_rs/build.rs
+++ driver_service_rs/build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-   tonic_build::configure()
+   tonic_prost_build::configure()
     .compile_protos(
         &["proto/trips/v1/trip.proto", "proto/trips/v1/trips_finder.proto"],
         &["proto"],
